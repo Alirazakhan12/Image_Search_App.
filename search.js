0fee34695b20cf4cb944e7377d3dc12ca30ec17f
Alirazakhan12 <raza346863@gmail.com>
2024-02-23 23:26:47 +0500

    updated

--- search.js
+++ search.js
@@ -1,53 +1,58 @@
+
 const AccessKey = "d85oLvsy6pXCTr0If5fI5ReA4yttNPZigSPm8EcVl08";
 
-const main_div = document.querySelector("#main");
-const form = document.querySelector("form");
-const search_input = document.querySelector("#search-input");
-const search_btn = document.querySelector("#search-btn");
-const Showmore = document.querySelector("#show_more");
+let form = document.querySelector("form");
+let search_btn = document.getElementById("search-btn");
+let search_input = document.getElementById("search-input");
+let main_div = document.getElementById("main");
+let Showmore = document.getElementById("show_more");
 
 let inputData = "";
 let page = 1;
 
-async function imageSearch() {
+async function search_img() {
   inputData = search_input.value;
+
+  if (page == 1) {
+   main_div.innerHTML = "";
+ }
+
   const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${AccessKey}`;
   let response = await fetch(url);
   let data = await response.json();
   let result = data.results;
+  // console.log(result);
 
-  if (page == 1) {
-    main_div.innerHTML = "";
-  }
 
   result.map((eve) => {
-    const div = document.createElement("div");
+    let div = document.createElement("div");
     div.classList.add("card");
-    const img = document.createElement("img");
-    img.src = eve.urls.small;
-    img.alt = eve.alt_description;
-    const link = document.createElement("a");
+    let image = document.createElement("img");
+    image.src = eve.urls.small;
+    image.alt = eve.alt_description;
+    let link = document.createElement("a");
     link.href = eve.links.html;
     link.target = "_blank";
     link.textContent = eve.alt_description;
 
-    div.appendChild(img);
+    div.appendChild(image);
     div.appendChild(link);
     main_div.appendChild(div);
   });
   page++;
 
   if (page > 1) {
-    Showmore.style.display = "block";
-  }
+   Showmore.style.display = "block";
+ }
 }
 
-form.addEventListener("submit", function (eve) {
+
+form.addEventListener("submit", (eve) => {
   eve.preventDefault();
-  page = 1;
-  imageSearch();
+  search_img();
 });
 
-Showmore.addEventListener("click", function () {
-  imageSearch();
-});
+
+Showmore.addEventListener("click",()=>{
+ search_img()
+})
